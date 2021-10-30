2972927bc97d7b312ccb219da0d26d1d09ea5c6c
Daniel <64423975+DanielrCardoso@users.noreply.github.com>
2021-10-29 23:46:02 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config():
     DEBUG = True
     MYSQL_HOST = "localhost"
     MYSQL_USER = "root"
-    MYSQL_PASSWORD = "BD4dmin1508"
+    MYSQL_PASSWORD = "senha"
     MYSQL_DB = "trabbd"
 
 config = {"gabconfig":Config()}
