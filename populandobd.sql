e89acf34fbd74705018852b423d0bc7db97e29d0
Daniel <64423975+DanielrCardoso@users.noreply.github.com>
2021-10-29 23:53:35 -0300

    Add files via upload

--- populandobd.sql
+++ populandobd.sql
@@ -2,15 +2,15 @@ insert into usuario (nome,user,senha,status_user,email) values("Daniel","@niel",
 insert into usuario (nome,user,senha,status_user,email) values("teste","@opa","senhaforte","Online","teste@eu");
 insert into usuario (nome,user,senha,status_user,email) values("outro","@other","senhaforte","Online","iai@eu");
 insert into usuario (nome,user,senha,status_user,email) values("dan","@dan","dan","Online","dan@eu");
-insert into usuario (nome,user,senha,status_user,email) values("bob","@cleiton","dog","Online","dog@auau");
+insert into usuario (nome,user,senha,status_user,email) values("bob","@cleiton","teste","Online","dog@auau");
 
 insert into administrador(nome,senha,email,cpf,data_contratacao) values ("eu","eu","eu@adm","11122233300","2001-04-13 08:04:02");
 insert into administrador(nome,senha,email,cpf,data_contratacao) values ("admin","admin","admin@adm","00000000000","2001-04-13 08:04:02");
-insert into administrador(nome,senha,email,cpf,data_contratacao) values ("danieladm","senhaforte","daniel@adm","000.111.222-33","2001-04-13 08:04:02");
-insert into administrador(nome,senha,email,cpf,data_contratacao) values ("gabsadm","senhaforte","gabs@adm","000.111.222-33","2001-04-13 08:04:02");
-insert into administrador(nome,senha,email,cpf,data_contratacao) values ("euadm","senhaforte","eu@adm","000.111.222-33","2001-04-13 08:04:02");
-insert into administrador(nome,senha,email,cpf,data_contratacao) values ("tuadm","senhaforte","tu@adm","000.111.222-33","2001-04-13 08:04:02");
-insert into administrador(nome,senha,email,cpf,data_contratacao) values ("maisadm","senhaforte","plus@adm","000.111.222-33","2001-04-13 08:04:02");
+insert into administrador(nome,senha,email,cpf,data_contratacao) values ("danieladm","senhaforte","daniel@adm","00111122233","2001-04-13 08:04:02");
+insert into administrador(nome,senha,email,cpf,data_contratacao) values ("gabsadm","senhaforte","gabs@adm","00211122233","2001-04-13 08:04:02");
+insert into administrador(nome,senha,email,cpf,data_contratacao) values ("euadm","senhaforte","eu@adm","00411122233","2001-04-13 08:04:02");
+insert into administrador(nome,senha,email,cpf,data_contratacao) values ("tuadm","senhaforte","tu@adm","00511122233","2001-04-13 08:04:02");
+insert into administrador(nome,senha,email,cpf,data_contratacao) values ("maisadm","senhaforte","plus@adm","00611122233","2001-04-13 08:04:02");
 
 insert into jogo(nome_jogo,desenvolvedora,classificacao_indicativa,categoria,sinopse,data_lancamento,number_user,IDadministrador) values ("fort","epic","16","nao sei","pei pei pou pou","2015-01-01","5","1");
 insert into jogo(nome_jogo,desenvolvedora,classificacao_indicativa,categoria,sinopse,data_lancamento,number_user,IDadministrador) values ("lolzin","riot","16","nao sei","pei pei pou pou","2015-01-01","5","2");
@@ -36,15 +36,25 @@ insert into Biblioteca(nome_no_jogo,id_jogo,id_usuario) values ("@gabs","3","3")
 insert into Biblioteca(nome_no_jogo,id_jogo,id_usuario) values ("@gabs","4","3");
 insert into Biblioteca(nome_no_jogo,id_jogo,id_usuario) values ("@gabs","5","3");
 
+insert into Biblioteca(nome_no_jogo,id_jogo,id_usuario) values ("@dan","1","4");
+insert into Biblioteca(nome_no_jogo,id_jogo,id_usuario) values ("@dan","2","4");
+insert into Biblioteca(nome_no_jogo,id_jogo,id_usuario) values ("@dan","3","4");
+insert into Biblioteca(nome_no_jogo,id_jogo,id_usuario) values ("@dan","4","4");
+insert into Biblioteca(nome_no_jogo,id_jogo,id_usuario) values ("@dan","5","4");
+
 insert into historicoPartida(id_Part,hora_inicio,hora_fim,id_user,status_result,IDjogo,pontos) values ("0001","2021-09-01 20:20:20","2021-09-01 20:30:20","1","fim","1","9");
 insert into historicoPartida(id_Part,hora_inicio,hora_fim,id_user,status_result,IDjogo,pontos) values ("0001","2021-09-01 20:20:20","2021-09-01 20:30:20","2","fim","1","10");
-
 insert into historicoPartida(id_Part,hora_inicio,hora_fim,id_user,status_result,IDjogo,pontos) values ("0002","2020-09-01 20:20:20","2020-09-01 20:30:20","1","fim","2","9");
 insert into historicoPartida(id_Part,hora_inicio,hora_fim,id_user,status_result,IDjogo,pontos) values ("0002","2020-09-01 20:20:20","2020-09-01 20:30:20","3","fim","2","10");
 
 insert into jogo(nome_jogo,desenvolvedora,classificacao_indicativa,categoria,sinopse,data_lancamento,IDadministrador) values ('teste','teste','2','teste','teste','2021-10-30','1');
 
 call conquista("Teste campeao","1","1","1");
+call conquista("Teste campeao 2","1","1","1");
+call conquista("Teste campeao 3","1","1","1");
+call conquista("Teste campeao 4","1","1","1");
+call conquista("Teste campeao 5","1","1","1");
+
 select * from conquistas;
 
 select * from historicoPartida;
